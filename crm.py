c9d53d2c9f3946effb2f1115b5430061e68904bc
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-02-12 22:43:27 +0300

    29

--- crm.py
+++ crm.py
@@ -10,9 +10,27 @@ root.geometry("400x400")
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="root"
+    passwd="root",
+    database="codemy",
 )
 
-print(mydb)
+my_cursor = mydb.cursor()
+# my_cursor.execute("CREATE DATABASE codemy")
+
+# my_cursor.execute("SHOW DATABASES")
+# for db in my_cursor:
+#     print(db)
+
+# my_cursor.execute("""CREATE TABLE customers (
+#     first_name VARCHAR(255),
+#     last_name VARCHAR(255),
+#     zipcode INT(10),
+#     price_paid DECIMAL(10, 2),
+#     user_id INT AUTO_INCREMENT PRIMARY KEY
+# )""")
+
+my_cursor.execute("SELECT * FROM customers")
+for thing in my_cursor.description:
+    print(thing)
 
 root.mainloop()
